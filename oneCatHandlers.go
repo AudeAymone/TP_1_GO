2267e549380927cd40209f1bff10040d70b3cbcc
AudeAymone <124136709+AudeAymone@users.noreply.github.com>
2025-11-17 15:04:32 +0100

    Get & Delete methods added for 1 cat

--- oneCatHandlers.go
+++ oneCatHandlers.go
@@ -1,8 +1,30 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+)
+
 
 func getCat(req *http.Request) (int, any) {
-	Logger.Info("Cat not found")
-	return http.StatusNotFound, "Cat not found"
+	catID := req.PathValue("catId")
+	cat, exists := catsDatabase[catID]
+
+	if !exists {
+		return http.StatusNotFound, "Cat not found"
+	}
+
+	return http.StatusOK, cat
+}
+
+
+func deleteCat(req *http.Request) (int, any) {
+	catID := req.PathValue("catId")
+	_, exists := catsDatabase[catID]
+
+	if !exists {
+		return http.StatusNotFound, "Cat not found"
+	}
+
+	delete(catsDatabase, catID)
+	return http.StatusNoContent, nil
 }
